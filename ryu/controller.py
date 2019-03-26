e43d37ae5e6b38127dedca6c2d2967f0d48e516e
David Tang <dtangster@yahoo.com>
2019-03-26 04:16:30 -0700

    Rename Ryu's controller to DefaultController

--- ryu/controller.py
+++ ryu/controller.py
@@ -2,5 +2,5 @@ from ryu.app.simple_switch_13 import SimpleSwitch13
 from ryu.app.ofctl_rest import RestStatsApi
 
 
-class HW2(RestStatsApi, SimpleSwitch13):
+class DefaultController(RestStatsApi, SimpleSwitch13):
     pass
